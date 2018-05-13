1a82a5618e4584fbf0454094ac37c501b0e2084f
Enzhi Li <enzhi.li@ussuning.com>
2018-05-12 17:04:02 -0700

    A working example

--- prim.cpp
+++ prim.cpp
@@ -11,6 +11,16 @@
 #include <queue>
 using namespace std;
 
+template <typename T>
+void print_vector(const vector<T> &my_vector)
+{
+    for (int i = 0; i < my_vector.size(); ++i)
+    {
+        cout << my_vector[i] << ", ";
+    }
+    cout << endl;
+}
+
 vector<string> get_lines(string file_name)
 {
     ifstream ifile(file_name);
@@ -40,12 +50,71 @@ void print_lines(const vector<string> &lines)
     }
 }
 
+vector<string> split(const string &line)
+{
+    vector<string> result;
+    istringstream iss(line);
+
+    do 
+    {
+        string item;
+        iss >> item;
+        result.push_back(item);
+    }while(iss);
+    return result;
+}
+
+struct Vertex
+{
+    char vertex;
+    int color;
+    Vertex()
+    {
+        this->vertex = '0';
+        this->color = -1;
+    }
+    Vertex(char vertex, int color)
+    {
+        this->vertex = vertex;
+        this->color = color;
+    }
+    Vertex(const Vertex &parameter)
+    {
+        this->vertex = parameter.vertex;
+        this->color = parameter.color;
+    }
+    void set_color(int color)
+    {
+        assert(color == 0 || color == 1 || color == 2);
+        this->color = color;
+    }
+    bool operator== (const Vertex &that) const
+    {
+        return this->vertex == that.vertex;
+    }
+    const Vertex & operator= (const Vertex &that)
+    {
+        this->vertex = that.vertex;
+        this->color = that.color;
+        return *this;
+    }
+    string to_str() const
+    {
+        return string(1, this->vertex) + ", color = " + to_string(this->color);
+    }
+    friend ostream & operator<< (ostream &os, const Vertex &parameter)
+    {
+        os << parameter.vertex << ", color = " << parameter.color;
+        return os;
+    }
+};
+
 struct Edge
 {
-    char left;
-    char right;
+    Vertex left;
+    Vertex right;
     int weight;
-    Edge(char left, char right, int weight)
+    Edge(Vertex left, Vertex right, int weight)
     {
         this->left = left;
         this->right = right;
@@ -54,7 +123,7 @@ struct Edge
     string to_str() const
     {
         string s = "";
-        s += string(1, left) + " -- " + string(1, right) + ", weight = " + to_string(weight);
+        s += left.to_str() + " -- " + right.to_str() + ", weight = " + to_string(weight);
         return s;
     }
 
@@ -87,20 +156,6 @@ struct Edge
     }
 };
 
-vector<string> split(const string &line)
-{
-    vector<string> result;
-    istringstream iss(line);
-
-    do 
-    {
-        string item;
-        iss >> item;
-        result.push_back(item);
-    }while(iss);
-    return result;
-}
-
 template <typename T>
 unordered_set<T> vector_to_set(const vector<T> &my_vector)
 {
@@ -123,23 +178,27 @@ vector<T> set_to_vector(const unordered_set<T> &set)
     return result;
 }
 
-vector<char> get_vertices(string inputFileName)
+vector<Vertex> get_vertices(string inputFileName)
 {
     vector<string> lines = get_lines(inputFileName);
-    vector<char> vertices;
+    vector<Vertex> vertices;
+    unordered_set<char> vertex_set;
     for (int i = 0; i < lines.size(); ++i)
     {
         //cout << lines[i] << endl;
         vector<string> temp = split(lines[i]);
         int numberOfVertices = (temp.size()-1)/2;
-        vertices.push_back(temp[0][0]);
+        vertex_set.insert(temp[0][0]);
         for (int j = 0; j < numberOfVertices; ++j)
         {
-            vertices.push_back(temp[j+1][0]);
+            vertex_set.insert(temp[j+1][0]);
         }
     }
-    //return vertices;
-    return set_to_vector(vector_to_set(vertices));
+    for (auto ele: vertex_set)
+    {
+        vertices.push_back(Vertex(ele, 0));
+    }
+    return vertices;
 }
 
 vector<Edge> line_to_edge(const string &line)
@@ -149,7 +208,7 @@ vector<Edge> line_to_edge(const string &line)
     int numberOfEdges = (elements.size() - 1)/2;
     for (int i = 0; i < numberOfEdges; ++i)
     {
-        Edge edge(elements[0][0], elements[i+1][0], stoi(elements[i + 1 + numberOfEdges]));
+        Edge edge(Vertex(elements[0][0], 0), Vertex(elements[i+1][0], 0), stoi(elements[i + 1 + numberOfEdges]));
         edges.push_back(edge);
         //cout << edge.to_str() << endl;
     }
@@ -221,19 +280,19 @@ priority_queue<T, vector<T>, greater<T> > vector_to_priority_queue(const vector<
     return graph;
 }*/
 
-unordered_map<char, vector<char> > generate_graph(string inputFileName)
+unordered_map<char, vector<Vertex> > generate_graph(string inputFileName)
 {
-    unordered_map<char, vector<char> > graph;
+    unordered_map<char, vector<Vertex> > graph;
     vector<string> lines = get_lines(inputFileName);
     for (int i = 0; i < lines.size(); ++i)
     {
         string line = lines[i];
         vector<string> items = split(line);
         int numberOfAdjacentVertices = (items.size() - 1)/2;
-        vector<char> adjacentVertices;
+        vector<Vertex> adjacentVertices;
         for (int j = 0; j < numberOfAdjacentVertices; ++j)
         {
-            adjacentVertices.push_back(items[j+1][0]);
+            adjacentVertices.push_back(Vertex(items[j+1][0], 0));
         }
         graph[line[0]] = adjacentVertices;
     }
@@ -316,16 +375,6 @@ int find_index(const vector<T> &my_vector, T element)
     return -1;
 }
 
-bool has_unvisited(const vector<bool> &visited)
-{
-    for (int i = 0; i < visited.size(); ++i)
-    {
-        if (!visited[i])
-            return true;
-    }
-    return false;
-}
-
 template <typename T>
 void print_pq(T &pq)
 {
@@ -339,11 +388,10 @@ void print_pq(T &pq)
 class Graph
 {
     private:
-        vector<char> vertices;
+        vector<Vertex> vertices;
         vector<Edge> edges;
         //unordered_map<char, priority_queue<Edge, vector<Edge>, greater<Edge> > > graph;
-        unordered_map<char, vector<char> > graph;
-        vector<bool> visited;
+        unordered_map<char, vector<Vertex> > graph;
     public:
         //Graph(){}
         Graph(string inputFileName)
@@ -351,151 +399,221 @@ class Graph
             this->graph = generate_graph(inputFileName);
             this->vertices = get_vertices(inputFileName);
             this->edges = get_edges(inputFileName);
-            for (int i = 0; i < vertices.size(); ++i)
+            /*for (auto vertex: vertices)
             {
-                visited.push_back(false);
+                cout << vertex << endl;
             }
-        }
-
-        vector<Edge> find_mst(char source)
-        {
-            assert(contains(this->vertices, source));
-            vector<Edge> tree;
-            char current_vertex = source;
-            int current_index = find_index(this->vertices, source);
-            visited[current_index] = true;
-            queue<char> q;
-            q.push(source);
-            while(!q.empty())
+            for (auto edge: edges)
             {
-                current_vertex = q.front();
-                cout << "Current_vertex: " << current_vertex << endl;
-                current_index = find_index(vertices, current_vertex);
-                assert(current_index >= 0 && current_index < vertices.size());
-                cout << "Current_index: " << current_index << endl;
-                assert(current_index >= 0);
-                visited[current_index] = true;
-                vector<char> adjacentVertices = graph[current_vertex];
-                for (int i = 0; i < adjacentVertices.size(); ++i)
+                cout << edge << endl;
+            }
+            for (auto pair: graph)
+            {
+                char vertex = pair.first;
+                vector<Vertex> adjacentVertices = pair.second;
+                cout << string(1, vertex) + " -> ";
+                for (auto ele: adjacentVertices)
                 {
-                    if (!visited[find_index(this->vertices, adjacentVertices[i])])
-                    {
-                        q.push(adjacentVertices[i]);
-                    }
-                }
-                for (char ele: adjacentVertices)
-                {
-                    cout << ele << ", ";
+                    cout << ele << "; ";
                 }
                 cout << endl;
-                vector<Edge> adjacentEdges;
-                for (int i = 0; i < adjacentVertices.size(); ++i)
+            }*/
+        }
+
+        vector<Vertex *> find_adjacent_vertices(Vertex *source)
+        {
+            vector<Vertex> adjacentVertices = graph[source->vertex];
+            vector<int> adjacentIndices;
+            for (auto vertex: adjacentVertices)
+            {
+                adjacentIndices.push_back(find_index(this->vertices, vertex));
+            }
+            vector<Vertex *> result;
+            for (int index: adjacentIndices)
+            {
+                result.push_back(&vertices[index]);
+            }
+            return result;
+        }
+
+        vector<Vertex *> find_adjacent_unvisited_vertices(Vertex *source)
+        {
+            vector<Vertex> adjacentVertices = graph[source->vertex];
+            vector<int> adjacentIndices;
+            for (auto ele: adjacentVertices)
+            {
+                adjacentIndices.push_back(find_index(this->vertices, ele));
+            }
+            vector<Vertex *> adjacentUnvisitedVertices;
+            for (auto index: adjacentIndices)
+            {
+                if (vertices[index].color == 0)
                 {
-                    char temp_vertex = adjacentVertices[i];
-                    int temp_index = find_index(vertices, temp_vertex);
-                    assert(temp_index >= 0);
-                    //cout << temp_vertex << ", " << temp_index << endl;
-                    if (true)
+                    adjacentUnvisitedVertices.push_back(&vertices[index]);
+                }
+            }
+            return adjacentUnvisitedVertices;
+        }
+
+        vector<Edge *> find_adjacent_unvisited_edges(Vertex *source)
+        {
+            vector<Vertex *> adjacentUnvisitedVertices = find_adjacent_unvisited_vertices(source);
+            vector<Edge *> adjacentUnvisitedEdges;
+            for (Vertex * vertex: adjacentUnvisitedVertices)
+            {
+                Edge edge = Edge(*source, *vertex, 0);
+                int edge_index = find_index(edges, edge);
+                adjacentUnvisitedEdges.push_back(&edges[edge_index]);
+            }
+            return adjacentUnvisitedEdges;
+        }
+
+        int get_weight(const Vertex &a, const Vertex &b)
+        {
+            Edge edge = Edge(a, b, 0);
+            for (int i = 0; i < edges.size(); ++i)
+            {
+                if (edge == edges[i])
+                    return edges[i].weight;
+            }
+            return -1;
+        }
+
+        vector<Edge> get_tree_adjacent_edges(const vector<Vertex> &inside, const vector<Vertex> &outside)
+        {
+            vector<Edge> result;
+            assert(inside.size() + outside.size() == vertices.size());
+            for (int i = 0; i < inside.size(); ++i)
+            {
+                vector<Vertex> adjacentVertices = graph[inside[i].vertex];
+                for (auto vertex: adjacentVertices)
+                {
+                    if (contains(outside, vertex))
                     {
-                        Edge edge(current_vertex, temp_vertex, 0);
-                        int edge_index = find_index(this->edges, edge);
-                        /*cout << edge << endl;
-                        cout << "All edges: " << endl;
-                        for (Edge ele: edges)
-                        {
-                            cout << ele << endl;
-                        }
-                        cout << "edge_index: " << edge_index << endl;*/
-                        assert(edge_index >= 0 && edge_index < edges.size());
-                        adjacentEdges.push_back(this->edges[edge_index]);
+                        Edge edge(inside[i], vertex, 0);
+                        int edge_index = find_index(edges, edge);
+                        if (!contains(result, edges[edge_index]))
+                            result.push_back(edges[edge_index]);
                     }
                 }
-                cout << "All adjacent edges: " << endl;
-                for (Edge ele: adjacentEdges)
+            }
+            return result;
+        }
+
+        vector<Edge> find_mst()
+        {
+            vector<Edge> tree;
+            vector<Vertex> inside;
+            vector<Vertex> outside;
+            for (int i = 0; i < vertices.size(); ++i)
+            {
+                outside.push_back(vertices[i]);
+            }
+            for (int i = 0; i < outside.size(); ++i)
+            {
+                cout << outside[i] << ", ";
+            }
+            cout << endl;
+            inside.push_back(outside.front());
+            outside.erase(outside.begin());
+            while(!outside.empty())
+            {
+                //cout << "Number of outside vertices = " << outside.size() << endl;
+                cout << "Inside vertices: " << endl;
+                print_vector(inside);
+                cout << "Outside vertices: " << endl;
+                print_vector(outside);
+                vector<Edge> tree_adjacent_edges = get_tree_adjacent_edges(inside, outside);
+                cout << "Adjacent edges of current tree: " << endl;
+                for (int i = 0; i < tree_adjacent_edges.size(); ++i)
                 {
-                    cout << ele << endl;
+                    cout << tree_adjacent_edges[i] << endl;
                 }
-                if(adjacentEdges.size() == 0)
+                if (tree_adjacent_edges.size() == 0)
                 {
-                    q.pop();
+                    outside.erase(outside.begin());
+                    break;
                 }
                 else
                 {
-                    int min_weight = adjacentEdges[0].weight;
+                    int min_weight = tree_adjacent_edges[0].weight;
                     int min_index = 0;
-                    for (int i = 0; i < adjacentEdges.size(); ++i)
+                    for (int i = 1; i < tree_adjacent_edges.size(); ++i)
                     {
-                        if (min_weight > adjacentEdges[i].weight)
+                        Edge edge = tree_adjacent_edges[i];
+                        if (edge.weight < min_weight)
                         {
-                            min_weight = adjacentEdges[i].weight;
+                            min_weight = edge.weight;
                             min_index = i;
                         }
                     }
-                    tree.push_back(adjacentEdges[min_index]);
-                    Edge min_edge = adjacentEdges[min_index];
-                    cout << "min_edge: " << endl;
-                    cout << min_edge << endl;
-                    if (current_vertex == min_edge.left)
+                    Edge min_edge = tree_adjacent_edges[min_index];
+                    tree.push_back(min_edge);
+                    cout << "min_edge to be added to the tree: " << min_edge << endl;
+                    bool add_left;
+                    if (contains(inside, min_edge.left))
                     {
-                        //q.push(min_edge.right);
-                        visited[find_index(vertices, min_edge.right)] = true;
-                    }
-                    else if (current_index == min_edge.right)
-                    {
-                        //q.push(min_edge.left);
-                        visited[find_index(vertices, min_edge.left)] = true;
+                        assert(!contains(inside, min_edge.right));
+                        inside.push_back(min_edge.right);
+                        add_left = false;
                     }
                     else
                     {
-                        cout << "Error in the min_edge for vertex " << current_vertex << endl;
-                        exit(-1);
+                        assert(!contains(inside, min_edge.left));
+                        inside.push_back(min_edge.left);
+                        add_left = true;
                     }
-                    q.pop();
+                    if (add_left)
+                        outside.erase(outside.begin() + find_index(outside, min_edge.left));
+                    else
+                        outside.erase(outside.begin() + find_index(outside, min_edge.right));
                 }
             }
             return tree;
         }
-
-        void bfs(char source)
+       
+        void bfs(Vertex source)
         {
             assert(contains(vertices, source));
-            queue<char> q;
-            q.push(source);
-            visited[find_index(vertices, source)] = true; 
+            int source_index = find_index(vertices, source);
+            assert(vertices[source_index].color == 0);
+            queue<Vertex *> q;
+            q.push(&vertices[source_index]);
+            q.front()->color = 1;
             while(!q.empty())
             {
-                char current_vertex = q.front();
-                visited[find_index(vertices, current_vertex)] = true;
-                cout << current_vertex << endl;
-                vector<char> adjacentVertices = graph[current_vertex];
-                for (int i = 0; i < adjacentVertices.size(); ++i)
+                Vertex * temp = q.front();
+                cout << *temp << endl;
+                temp->color = 2;
+                vector<Vertex *> adjacentVertices = find_adjacent_vertices(temp);
+                for (auto ele: adjacentVertices)
                 {
-                    char temp_vertex = adjacentVertices[i];
-                    if (!visited[find_index(vertices, temp_vertex)])
+                    if (ele->color == 0)
                     {
-                        q.push(temp_vertex);
+                        q.push(ele);
+                        ele->color = 1;
                     }
                 }
                 q.pop();
             }
-        }
-
-        /*~Graph()
-        {
-            for (pair<char, priority_queue<Edge, vector<Edge>, greater<Edge> > > ele: graph)
+            /*cout << "Updated vertices: " << endl;
+            for (auto ele: vertices)
             {
-                cout << ele.first << " -> ";
-                print_pq(ele.second); 
-                cout << endl;
-            }
-        }*/
+                cout << ele << endl;
+            }*/
+        }
 };
 
 int main()
 {
     string inputFileName = "graph.txt";
     Graph graph = Graph(inputFileName);
-    //vector<Edge> tree = graph.find_mst('a');
-    graph.bfs('a');
+    vector<Edge> tree = graph.find_mst();
+    cout << "The minimum spanning tree: " << endl;
+    for (auto edge: tree)
+    {
+        cout << edge << endl;
+    }
+    //graph.bfs(Vertex('a', 0));
     return 0;
 }
